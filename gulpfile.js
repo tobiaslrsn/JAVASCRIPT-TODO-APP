01ea7500c7dbae43d94a37ff956484e746fcfdbd
tobiaslrsn <nestorxxv@gmail.com>
2021-11-02 13:58:45 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task("compile", function () {
+  return gulp.src("sass/main.scss").pipe(sass()).pipe(gulp.dest("css"));
+});
+
+gulp.task("watch-scss", function () {
+  gulp.watch("sass/**/*.scss", gulp.series("compile"));
+});
+
+gulp.task("default", gulp.series("compile", "watch-scss"), function () {});

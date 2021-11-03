cd13fd755734e37ecbe46e11564fd6a88431b783
tobiaslrsn <nestorxxv@gmail.com>
2021-11-03 15:37:08 +0100

    Added remove-function to list_items and eventlistener to innerHTML

--- js/main.js
+++ js/main.js
@@ -5,12 +5,21 @@ window.onload = function () {
 
   myToDoList();
 
-  function myToDoList() {
-    let listHeader = "En todo-lista";
-    let listHeaderH1 = document.createElement("h1");
-    listHeaderH1.className = "listHeaderH1";
-    listHeaderH1.innerHTML = listHeader;
+  start();
 
+  //övning 2 - print to innerHTML, bygga på en lista här?
+  function start() {
+    let calculateButton = document.getElementById("buttonToCalculate");
+    calculateButton.addEventListener("click", handleCalculation);
+  }
+  function handleCalculation() {
+    numberTwo = document.getElementById("secondNumberInput").value;
+    result = document.getElementById("result").innerHTML = numberTwo;
+    document.getElementById("result").innerHTML;
+  }
+
+  //hårdkodad lista
+  function myToDoList() {
     let myListOfTodos = [
       "list_item1",
       "list_item2",
@@ -18,7 +27,7 @@ window.onload = function () {
       "list_item4",
       "list_item5",
     ];
-
+    //tryck ut hårdkodade listan
     let myToDoListUl = document.createElement("ul");
     myToDoListUl.className = "listSection";
 
@@ -26,11 +35,28 @@ window.onload = function () {
       let myToDoListLi = document.createElement("li");
       myToDoListLi.className = "theList";
       myToDoListLi.innerHTML = myListOfTodos[i];
-
       myToDoListUl.appendChild(myToDoListLi);
     }
 
-    document.getElementById("startPageContainer").appendChild(listHeaderH1);
     document.getElementById("startPageContainer").appendChild(myToDoListUl);
+    let removeFromList = document.getElementsByTagName("li");
+
+    for (i = 0; i < removeFromList.length; i++) {
+      let span = document.createElement("span");
+      let txt = document.createTextNode("\u00D7");
+      span.className = "closeButton";
+      span.appendChild(txt);
+      removeFromList[i].appendChild(span);
+    }
+
+    let closeButton = document.getElementsByClassName("closeButton");
+    for (i = 0; i < closeButton.length; i++) {
+      closeButton[i].onclick = function () {
+        let div = this.parentElement;
+        div.style.display = "none";
+
+        console.log("Task is done and gone.");
+      };
+    }
   }
 };

2abca4470890f93410699099d0c88fda64249174
tobiaslrsn <nestorxxv@gmail.com>
2021-11-04 19:29:04 +0100

    renamed variables and functions + added some style

--- js/main.js
+++ js/main.js
@@ -1,80 +1,81 @@
 window.onload = function () {
-  let startPageContainer = document.createElement("div");
-  startPageContainer.id = "startPageContainer";
-  document.body.appendChild(startPageContainer);
+  let toDoListContainer = document.createElement("div");
+  toDoListContainer.id = "startPageContainer";
+  document.body.appendChild(toDoListContainer);
 
-  handleCalculation();
-  myToDoList();
+  handleAddToList();
+  theBeautifulToDoList();
 
-  //övning 2 - print to innerHTML, bygga på en lista här?
-  /*   function start() {
-    
-  } */
-  /* function handleCalculation() {
-    let calculateButton = document.getElementById("toDo_add-btn");
-    calculateButton.addEventListener("click", handleCalculation);
+  function handleAddToList() {
+    let addInputValueToListButton = document.getElementById("toDo_add-btn");
+    addInputValueToListButton.addEventListener("click", handleAddToList);
 
-    let addInputToList = document.getElementById("toDo_inputField").value;
-    let personsAddedToDo = (document.getElementById("addedToDo").innerHTML =
-      addInputToList);
+    let addInputValueToList = document.getElementById("toDo_inputField").value;
+    let whatsBeenAddedToList = (document.getElementById("addedToDo").innerHTML =
+      addInputValueToList);
+
     document.getElementById("addedToDo").innerHTML;
-    console.log(personsAddedToDo);
-  } */
+    console.log(whatsBeenAddedToList);
+  }
 
-  function handleCalculation(myListOfTodos) {
+  function handleAddToList(addListItems) {
     let getInputValue = document.getElementById("toDo_inputField");
     let getInputButton = document.getElementById("toDo_add-btn");
 
     getInputButton.addEventListener("click", () => {
       let myNewValue = getInputValue.value;
-      myListOfTodos.push(myNewValue);
-      myToDoList();
-      console.log(myListOfTodos);
+      addListItems.innerHTML = myNewValue;
+
+      theBeautifulToDoList();
+      console.log(addListItems);
     });
   }
+
   //hårdkodad lista
-  function myToDoList() {
-    let myListOfTodos = [
+  function theBeautifulToDoList() {
+    let addListItems = [
       "list_item1",
       "lo",
-      "list_ifasgjdjs ddjjdd dddjjdjd djdjdj asgjajgjag agjsjgbdugsdg gsdgjsdj djdjtem3",
+      "list_ifasgjdjs ddjjdd dddjjdjd agjsjgbdugsdg gsdgjsdj djdjtem3",
       "list_item4",
       "list_item5",
     ];
-    handleCalculation(myListOfTodos);
+    handleAddToList(addListItems);
+
     //tryck ut hårdkodade listan
-    let myToDoListUl = document.createElement("ul");
-    myToDoListUl.className = "listSection";
+    let listContainerUl = document.createElement("ul");
+    listContainerUl.className = "listSection";
 
-    for (let i = 0; i < myListOfTodos.length; i++) {
-      let myToDoListLi = document.createElement("li");
-      myToDoListLi.className = "theList";
-      myToDoListLi.innerHTML = myListOfTodos[i];
-      myToDoListUl.appendChild(myToDoListLi);
+    for (let i = 0; i < addListItems.length; i++) {
+      let listContainerContentLi = document.createElement("li");
+      listContainerContentLi.className = "theList";
+      listContainerContentLi.innerHTML = addListItems[i];
+      listContainerUl.appendChild(listContainerContentLi);
+    }
+    document.getElementById("startPageContainer").appendChild(listContainerUl);
+
+    //Ta bort från listan
+    let removeListItems = document.getElementsByTagName("li");
+
+    for (i = 0; i < removeListItems.length; i++) {
+      let closeButton = document.createElement("div");
+      let clickSectionToRemoveListItems = document.createElement("p");
+      clickSectionToRemoveListItems.innerHTML = "DONE";
+      closeButton.className = "closeButton";
+
+      closeButton.appendChild(clickSectionToRemoveListItems);
+      removeListItems[i].appendChild(closeButton);
     }
 
-    document.getElementById("startPageContainer").appendChild(myToDoListUl);
-    let removeFromList = document.getElementsByTagName("li");
-
-    for (i = 0; i < removeFromList.length; i++) {
-      let div = document.createElement("div");
-      let deleteSymbol = document.createElement("p");
-      deleteSymbol.innerHTML = "DONE";
-      div.className = "closeButton";
-      div.appendChild(deleteSymbol);
-      removeFromList[i].appendChild(div);
-    }
-
-    let closeButton = document.getElementsByClassName("closeButton");
-    for (i = 0; i < closeButton.length; i++) {
-      closeButton[i].onclick = function () {
-        let div = this.parentElement;
-        div.style.display = "none";
+    let onClickRemove = document.getElementsByClassName("closeButton");
+    for (i = 0; i < onClickRemove.length; i++) {
+      onClickRemove[i].onclick = function () {
+        let closeButton = this.parentElement;
+        closeButton.style.display = "none";
 
         console.log("Task is done and gone.");
       };
     }
-
-    //Skriv om på Mitt sätt:
   }
+  //Skriv om på Mitt sätt:
 };

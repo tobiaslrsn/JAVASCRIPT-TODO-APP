ca1e8847d4b00041b41c4cea22f2aa531345ea58
tobiaslrsn <nestorxxv@gmail.com>
2021-11-12 15:07:54 +0100

    minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require("gulp");
-var sass = require("gulp-sass");
+var sass = require("gulp-sass")(require("sass"));
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
 
 gulp.task("compile", function () {
-  return gulp.src("sass/main.scss").pipe(sass()).pipe(gulp.dest("css"));
+  return gulp.src("sass/*.scss").pipe(sass()).pipe(gulp.dest("css"));
 });
 
-gulp.task("watch-scss", function () {
+gulp.task("minify", function () {
+  return gulp
+    .src("./css/main.css")
+    .pipe(csso())
+    .pipe(rename("main.min.css"))
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("watch-sass", function () {
   gulp.watch("sass/**/*.scss", gulp.series("compile"));
 });
 
-gulp.task("default", gulp.series("compile", "watch-scss"), function () {});
+gulp.task("default", gulp.series("compile", "watch-sass"), function () {});
